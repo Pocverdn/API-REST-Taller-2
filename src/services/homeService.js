ddd5e1bd9d1f9cf0e30ba494ab148982492bbb21
Pocverdn <santisanches538@gmail.com>
2025-10-29 16:54:04 -0500

    Segundo intento

--- src/services/homeService.js
+++ src/services/homeService.js
@@ -1,5 +1,5 @@
 const axios = require('axios');
-const SPRING_BOOT_API_URL = process.env.SPRING_BOOT_API_URL || 'http://localhost:8080/api';
+const SPRING_BOOT_API_URL = 'http://172.31.30.92:8080/api';
 
 const getHome = async (token) => {
 
